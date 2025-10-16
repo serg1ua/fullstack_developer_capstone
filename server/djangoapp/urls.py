00c484f383c56f4a45a115595f59a9f260cb0771
Serhii Zobov <marxser.ua@gmail.com>
2025-10-16 09:44:42 +0300

    Feat/dynamic (#2)
    * user login signup
    
    * refactor dynamic pages
    
    * refactor dynamic pages

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,15 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
 
-app_name = 'djangoapp'
+from . import views
+
+app_name = "djangoapp"
 urlpatterns = [
-    # # path for registration
-
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
+    path("login", views.UserLoginView.as_view(), name="login"),
+    path("logout", views.UserLoginView.as_view(), name="logout"),
+    path("register", views.UserRegistrationView.as_view(), name="login"),
+    # path("login", TemplateView.as_view(template_name="index.html")),
     # path for dealer reviews view
-
     # path for add a review view
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
